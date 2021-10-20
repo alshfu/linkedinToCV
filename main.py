57db5bb4756cdbdeea55172a4d95bc5f667107c0
alshfu <alshfu@gmail.com>
2021-10-20 08:20:29 +0200

    bug fix

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
-
-# Press Skift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from linkedinScraper import Person
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    login = ''
+    password = ''
+    url_page = ''
+    person = Person(login_name=login, pwd=password, url=url_page)
+    # person.get_profile_image()
+    person.create_json_file()
+    person.driver_close()
